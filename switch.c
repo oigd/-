b0f07673c7748b9029c9895318d24905dc6f3412
oigd <115806760+oigd@users.noreply.github.com>
2022-11-02 17:35:59 +0800

    oigd

---
+++ switch.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main(){
+ 	int c;
+ 	scanf ("%d",&c);
+ 	switch (c){
+ 		case 1 :printf("a")
+ 		;break
+ 		;case 2 :printf("b")
+ 		;break
+ 		;case 3 :printf("c")
+ 		;break ;
+		 default :printf("d");
+ }} 
+
